9c77f935558fdc76b4e63429021f16eeb6805c4f
SaurabPoudel <poudelsaurab20@gmail.com>
2024-10-15 13:08:40 +0545

    linux distro

--- src/main.zig
+++ src/main.zig
@@ -1,22 +1,38 @@
 const std = @import("std");
 
-fn getOsName() []const u8 {
+fn getOsName() ![]const u8 {
     const os = @import("builtin").os;
 
-    switch (os.tag) {
-        .linux => return "Linux",
-        .macos => return "macOs",
-        .windows => return "Windows",
-        .freebsd => return "FreeBSD",
-        else => return "Unknown OS",
+    return switch (os.tag) {
+        .linux => try getLinuxDistribution(),
+        .macos => "macOS",
+        .windows => "Windows",
+        .freebsd => "FreeBSD",
+        else => "Unknown OS",
+    };
+}
+
+fn getLinuxDistribution() ![]const u8 {
+    const file = try std.fs.cwd().openFile("/etc/os-release", .{});
+
+    var buffer: [1024]u8 = undefined;
+    const read_bytes = try file.readAll(buffer[0..]);
+
+    var os_release = std.mem.tokenizeAny(u8, buffer[0..read_bytes], "\n");
+
+    while (os_release.next()) |line| {
+        if (std.mem.startsWith(u8, line, "ID=")) {
+            return line[3..];
+        }
     }
+
+    return "Unknown Linux";
 }
 
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
     try stdout.print("**********NERD**FETCH****************\n", .{});
 
-    // OS
-    const os_name = getOsName();
-    try stdout.print("Operating System: {s} \n", .{os_name});
+    const os_name = try getOsName();
+    try stdout.print("Operating System: {s}\n", .{os_name});
 }
